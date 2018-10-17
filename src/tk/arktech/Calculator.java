d4a245b23a2ed69de8422d0b0da8b4cc9a10154c
Jakub Markowski <kuba.xx5@outlook.com>
2018-10-17 23:20:11 +0200

    Zrobiony Calculator

--- src/tk/arktech/Calculator.java
+++ src/tk/arktech/Calculator.java
@@ -1,12 +1,21 @@
 package tk.arktech;
 
 public class Calculator {
-    CurrencyCollection kolekcja;
 
-    //TODO zrobic prawdziwa metode
-    double GetResult(double kwota, String kod_waluty)
+
+    private CurrencyCollection kolekcja;
+
+    public Calculator(CurrencyCollection kolekcja) {
+        this.kolekcja = kolekcja;
+    }
+
+
+    double GetResult(double kwota, String from, String to)
     {
+        Currency cFrom = kolekcja.getByCode(from);
+        Currency cTo = kolekcja.getByCode(to);
 
-        return 0;
+        return ((kwota/cFrom.getPrzelicznik())/cFrom.getSredni_kurs()) * cTo.getPrzelicznik() * cTo.getSredni_kurs();
+
     }
 }
